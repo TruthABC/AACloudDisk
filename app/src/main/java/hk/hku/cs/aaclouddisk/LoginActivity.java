a405eb4ea4275e47531ac223c134ea686a681946
Shijian <liushi2@hotmail.com>
2018-11-27 04:13:01 +0800

    login page done except register

--- app/src/main/java/hk/hku/cs/aaclouddisk/LoginActivity.java
+++ app/src/main/java/hk/hku/cs/aaclouddisk/LoginActivity.java
@@ -1,14 +1,18 @@
 package hk.hku.cs.aaclouddisk;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -23,8 +27,8 @@ import hk.hku.cs.aaclouddisk.entity.response.CommonResponse;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //Local Data
-    private SharedPreferences sharedPreferences;
+    //Local State
+    SharedPreferences sharedPreferences;
 
     //View Widgets
     private Button mBtnLogin;
@@ -40,10 +44,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Local State load
+        sharedPreferences = getSharedPreferences("AACloudLogin", Context.MODE_PRIVATE);
+
         initViews();
         initToolBar();
         initEvents();
         initFinal();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mEditTextId.setText("");
+        mEditTextPassword.setText("");
+        mEditTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        mEditTextPassword.setTypeface(Typeface.DEFAULT);
+        loadAndSetCheckBoxState();
+        tryRecoverUserInput();
     }
 
     private void initViews() {
@@ -61,23 +79,124 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initEvents() {
+        //see password
+        mImageViewSeePw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleSeePw();
+            }
+        });
         //login button logic
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doAuthentication();
+                trySaveUserInput();
+                doAuthentication(mEditTextId.getText().toString(), mEditTextPassword.getText().toString());
+            }
+        });
+        //checkboxes check logic
+        mCheckBoxPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {   //if not "remember me", then must not "auto login"
+                    mCheckBoxLogin.setChecked(false);
+                }
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("RememberMe", mCheckBoxPassword.isChecked());
+                editor.putBoolean("AutoLogin", mCheckBoxLogin.isChecked());
+                editor.commit();
+            }
+        });
+        mCheckBoxLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {   //if "auto login", then must "remember me"
+                    mCheckBoxPassword.setChecked(true);
+                }
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("RememberMe", mCheckBoxPassword.isChecked());
+                editor.putBoolean("AutoLogin", mCheckBoxLogin.isChecked());
+                editor.commit();
             }
         });
     }
 
     private void initFinal() {
+        tryAutoLogin();
+    }
 
+    /**
+     *  To auto log user in, called in onCreate last
+     *   depends on "Auto Login"
+     */
+    private void tryAutoLogin() {
+        if (sharedPreferences.getBoolean("AutoLogin", true)) {
+            String id = sharedPreferences.getString("id","");
+            String password = sharedPreferences.getString("password","");
+            if (id != null && password != null && id.length()>0 && password.length()>0) {
+                doAuthentication(id, password);
+            }
+        }
+    }
+
+    /**
+     *  To auto fill id and password, called in onResume
+     */
+    private void tryRecoverUserInput() {
+        mEditTextId.setText(sharedPreferences.getString("id",""));
+        mEditTextPassword.setText(sharedPreferences.getString("password",""));
+    }
+
+    /**
+     *  To cache user id and password, called when login clicked
+     *   depends on Remember Me
+     */
+    private void trySaveUserInput() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (mCheckBoxPassword.isChecked()) {
+            editor.putString("id", mEditTextId.getText().toString());
+            editor.putString("password", mEditTextPassword.getText().toString());
+        } else {
+            editor.putString("id", "");
+            editor.putString("password", "");
+        }
+        editor.commit();
+    }
+
+    /**
+     * Load Local state of Remember me and Auto login
+     */
+    public void loadAndSetCheckBoxState() {
+        //get check state
+        boolean rememberMeState = sharedPreferences.getBoolean("RememberMe", true);
+        boolean autoLoginState = sharedPreferences.getBoolean("AutoLogin", true);
+
+        //set check state
+        mCheckBoxPassword.setChecked(rememberMeState);
+        mCheckBoxLogin.setChecked(autoLoginState);
+    }
+
+    //Visibility of password
+    private boolean passwordSeeFlag = false;
+    private void toggleSeePw() {
+        //Toggle Visibility
+        if (!passwordSeeFlag) {
+            mEditTextPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        } else {
+            mEditTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            mEditTextPassword.setTypeface(Typeface.DEFAULT);
+        }
+        passwordSeeFlag = !passwordSeeFlag;
+
+        //Focus
+        mEditTextPassword.clearFocus();
+        mEditTextPassword.requestFocus();
+        //Index to Last Word Input
+        mEditTextPassword.setSelection(mEditTextPassword.getText().toString().length());
     }
 
     //login button logic
-    private void doAuthentication() {
-        final String id = mEditTextId.getText().toString().trim();
-        final String password = mEditTextPassword.getText().toString().trim();
+    private void doAuthentication(final String id, final String password) {
 
         //should not be empty
         if (id.equals("")) {
@@ -102,8 +221,8 @@ public class LoginActivity extends AppCompatActivity {
             public void run() {
                 String url = HttpUtilsHttpURLConnection.BASE_URL + "/login";
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("id",id);
-                params.put("password",password);
+                params.put("id", id);
+                params.put("password", password);
 
                 String response = HttpUtilsHttpURLConnection.getContextByHttp(url,params);
 
@@ -135,7 +254,7 @@ public class LoginActivity extends AppCompatActivity {
                                 //go to main page
                                 loginAndForward();
                             } else {
-                                showToast("Login Failed");
+                                showToast("Login Failed: " + response.getErrmsg());
                             }
                         } catch (JsonSyntaxException e) {
                             showToast("Network error, plz contact maintenance.");
--- app/src/main/java/hk/hku/cs/aaclouddisk/main/tab/MeFragment.java
+++ app/src/main/java/hk/hku/cs/aaclouddisk/main/tab/MeFragment.java
@@ -5,15 +5,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import hk.hku.cs.aaclouddisk.R;
 
 public class MeFragment extends Fragment {
 
+    private Button mBtnLogout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_me, container, false);
+
+        initViews(rootView);
+        initEvents();
+        initFinal();
+
         return rootView;
     }
 
+    private void initViews(View v) {
+        mBtnLogout = v.findViewById(R.id.btn_logout);
+    }
+
+    private void initEvents() {
+        mBtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutAndForward();
+            }
+        });
+    }
+
+    private void initFinal() {
+
+    }
+
+    private void logoutAndForward() {
+        getActivity().finish();
+    }
 }
